b0a93bdbc934d395637113ef77071b764bec13c2
wcl <393928598@qq.com>
2017-03-18 17:19:36 +0800

    header组件

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,23 +1,66 @@
 <template>
-	<div class="header">
-		<div class="content_wrapper">
+  <div class="header">
+    <div class="content-wrapper">
       <div class="avatar">
-        <img width="64" height="64" :src="seller.avatar">
+        <img class="img-avatar" width="64" height="64" :src="seller.avatar">
       </div>
-      <div class="bulletin-wrapprt"></div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">
+          {{seller.description}}/{{seller.deliveryTime}}分钟送达
+        </div>
+        <div v-if="seller.supports" class="support">
+          <span class="icon"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
+      </div>
+      <div class="bulletin-wrapprt">
+      </div>
     </div>
-	</div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-	export default {
-	    props: {
-        seller: {
-	            type: Object
-          }
+  export default {
+    props: {
+      seller: {
+        type: Object
       }
+    }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+
+  .header
+    color: #ffffff
+    background: #000
+    .content-wrapper
+      padding: 24px 12px 18px 24px
+      font-size: 0px
+      .avatar
+        display: inline-block
+        .img-avatar
+          border-radius: 4px
+      .content
+        display: inline-block
+        margin-left: 16px
+        font-size: 14px
+        .title
+          margin: 2px 0 8px 0
+          .brand
+            display: inline-block
+            width: 30px
+            height: 18px
+            bg-url('brand')
+            background-size: 30px 18px
+            background-repeat: no-repeat
+            .name
+              margin-left: 6px
+              font-size: 16px
+
 </style>
