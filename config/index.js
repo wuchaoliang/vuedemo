e9ad1e46c3b4714435ab03199215e683b6446914
wcl <393928598@qq.com>
2017-03-20 14:04:54 +0800

    修改端口名

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 9000,
+    port: 7070,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
