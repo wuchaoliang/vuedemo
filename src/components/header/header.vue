648071ca8701c6be906f009b24c61cfef4dd6bac
wcl <393928598@qq.com>
2017-03-22 09:12:44 +0800

    header组件

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -29,7 +29,7 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%">
     </div>
-    <div v-show="detailShow" class="detail">
+    <div v-show="detailShow" class="detail" transition="fade">
       <div class="detail-wrapper clearfix">
         <div class="detail-main">
           <h1 class="name">{{seller.name}}</h1>
@@ -41,15 +41,23 @@
             <div class="text">用户信息</div>
             <div class="line"></div>
           </div>
-          <ul class="supports">
-            <li v-for="item in seller.supports" class="support-item">
-              <span class="icon" :class="classMap[seller.supports[$index].type]"></span>
-              <span class="text" :class="classMap[seller.supports[$index].description]"></span>
-            </li>
-          </ul>
+          <!--<ul class="supports">-->
+            <!--<li v-for="item in seller.supports" class="support-item">-->
+              <!--<span class="icon" :class="classMap[seller.supports[$index].type]"></span>-->
+              <!--<span class="text" :class="classMap[seller.supports[$index].description]"></span>-->
+            <!--</li>-->
+          <!--</ul>-->
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">商家公告</div>
+            <div class="line"></div>
+          </div>
+          <div class="bulletin">
+            <p class="cintent">{{seller.bulletin}}</p>
+          </div>
         </div>
       </div>
-      <div class="detail-close">
+      <div class="detail-close" @click="hideDetail">
         <i class="icon-close"></i>
       </div>
     </div>
@@ -72,6 +80,9 @@
     methods: {
       showDetail() {
         this.detailShow = true
+      },
+      hideDetail() {
+        this.detailShow = false
       }
     },
     created() {
@@ -206,7 +217,14 @@
       width: 100%
       height: 100%
       overflow: auto
-      background: rgba(7,17,27,0.8)
+      transition: all 2
+      background-filter: blur(10px)
+      &.fade-transition
+        opacity: 1
+        background: rgba(7, 17, 27, 0.8)
+      &.fade-enter, &fade-leave
+        opacity: 0
+        background: rgba(7, 17, 27, 0)
       .detail-wrapper
         min-height: 100%
         width: 100%
@@ -236,6 +254,44 @@
               padding: 0 12px
               font-weight: 700
               font-size: 14px
+
+          .supports
+            width: 80%
+            margin: 0 auto
+            .supports-item
+              padding: 0 12px
+              margin-bottom: 12px
+              font-size: 0
+              &:last-child
+                margin-bottom: 0
+              .icon
+                display: inline-block
+                width: 16px
+                height: 16px
+                vertical-align: top
+                margin-right: 6px
+                background-size: 16px 16px
+                background-repeat: no-repeat
+                &.decrease
+                  bg-url('decrease_2')
+                &.discount
+                  bg-url('discount_2')
+                &.guarantee
+                  bg-url('guarantee_2')
+                &.invoice
+                  bg-url('invoice_2')
+                &.special
+                  bg-url('special_2')
+              .text
+                line-height: 12px
+                font-size: 12px
+          .bulletin
+            width: 80%
+            margin: 0 auto
+            .content
+              padding: 0 12px
+              line-height: 24px
+              font-size: 12px
       .detail-close
         position: relative
         width: 32px
