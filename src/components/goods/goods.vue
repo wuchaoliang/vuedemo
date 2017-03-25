94e4beb26bb72a1a4d8c4317468c5a292412ea9d
wcl <393928598@qq.com>
2017-03-25 15:17:46 +0800

    左边菜单

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,8 +1,17 @@
 <template>
-	<div class="goods">
-    <div class="menu-wrapper"></div>
+  <div class="goods">
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="(item,index) in goods" class="menu-item">
+          <span class="text border-1px">
+            <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
+            {{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
     <div class="foods-wrapper"></div>
-	</div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
@@ -19,6 +28,7 @@
       }
     },
     created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
       this.$http.get('./api/goods').then((res) => {
         res = res.body;
         if (res.errno === ERR_OK) {
@@ -31,6 +41,7 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
   .goods
     display: flex
     position: absolute
@@ -41,6 +52,36 @@
     .menu-wrapper
       flex: 0 0 80px
       background: #f3f5f7
+      .menu-item
+        display: table
+        height: 54px
+        width: 56px
+        line-height: 14px
+        padding: 0 12px
+        .icon
+          display: inline-block
+          width: 12px
+          height: 12px
+          vertical-align: top
+          margin-right: 2px
+          background-size: 12px 12px
+          background-repeat: no-repeat
+          &.decrease
+           bg-url('decrease_3')
+          &.discount
+           bg-url('discount_3')
+          &.guarantee
+           bg-url('guarantee_3')
+          &.invoice
+           bg-url('invoice_3')
+          &.special
+           bg-url('special_3')
+        .text
+          display: table-cell
+          width: 56px
+          vertical-align: middle
+          border-1px(rgba(7,17,27,0.1))
+          font-size: 12px
     .foods-wrapper
       flex: 1
 </style>
