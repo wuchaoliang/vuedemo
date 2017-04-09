b6de57a2e58ebe1a16d4a7020812e6df66ced180
Pepper <393928598@qq.com>
2017-04-09 17:19:32 +0800

    菜单滚动

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,40 +1,42 @@
 <template>
-  <div class="goods">
-    <div ref="menuWrapper" class="menu-wrapper">
-      <ul>
-        <li v-for="(item,index) in goods" class="menu-item" :class="{'current':currentIndex===index}" @click="selectMenu(index,$enent)">
+  <div>
+    <div class="goods">
+      <div ref="menuWrapper" class="menu-wrapper">
+        <ul>
+          <li v-for="(item,index) in goods" class="menu-item" :class="{'current':currentIndex===index}" @click="selectMenu(index,$enent)">
           <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
             {{item.name}}
           </span>
-        </li>
-      </ul>
-    </div>
-    <div ref="foodsWrapper" class="foods-wrapper">
-      <ul>
-        <li v-for="(item,index) in goods" class="food-list food-list-hook">
-          <h1 class="title">{{item.name}}</h1>
-          <ul>
-            <li v-for="(food,index) in item.foods" class="food-item border-1px">
-              <div class="icon">
-                <img width="57" height="57" :src="food.icon">
-              </div>
-              <div class="content">
-                <h2 class="name">{{food.name}}</h2>
-                <p class="desc">{{food.description}}</p>
-                <div class="extra">
-                  <span class="count">月售{{food.sellCount}}份</span>
-                  <span>好评率{{food.rating}}%</span>
+          </li>
+        </ul>
+      </div>
+      <div ref="foodsWrapper" class="foods-wrapper">
+        <ul>
+          <li v-for="(item,index) in goods" class="food-list food-list-hook">
+            <h1 class="title">{{item.name}}</h1>
+            <ul>
+              <li v-for="(food,index) in item.foods" class="food-item border-1px">
+                <div class="icon">
+                  <img width="57" height="57" :src="food.icon">
                 </div>
-                <div class="price">
-                  <span class="now">￥{{food.price}}</span>
-                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                <div class="content">
+                  <h2 class="name">{{food.name}}</h2>
+                  <p class="desc">{{food.description}}</p>
+                  <div class="extra">
+                    <span class="count">月售{{food.sellCount}}份</span>
+                    <span>好评率{{food.rating}}%</span>
+                  </div>
+                  <div class="price">
+                    <span class="now">￥{{food.price}}</span>
+                    <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  </div>
                 </div>
-              </div>
-            </li>
-          </ul>
-        </li>
-      </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -61,7 +63,6 @@
         res = res.body;
         if (res.errno === ERR_OK) {
           this.goods = res.data;
-          console.log(this.goods);
           this.$nextTick(function() {
             this._initScroll();
             this._calHeight();
@@ -71,7 +72,7 @@
     },
     computed: {
       currentIndex() {
-        for (let i = 0; i < this.listHeight; i++) {
+        for (let i = 0; i < this.listHeight.length; i++) {
           let height1 = this.listHeight[i];
           let height2 = this.listHeight[i + 1];
           if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
@@ -97,6 +98,7 @@
         });
         this.foodsScroll.on('scroll', (pos) => {
           this.scrollY = Math.abs(Math.round(pos.y));
+          console.log(this.scrollY);
         })
       },
       _calHeight() {
@@ -133,6 +135,7 @@
         padding: 0 12px
         &.current
           position: relative
+          z-index: 10
           margin-top: -1px
           background: #fff
           font-weight: 700
