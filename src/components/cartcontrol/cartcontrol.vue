b0e55494f1aa36447a577003587d6eb6ea0673fe
Pepper <393928598@qq.com>
2017-04-25 22:34:56 +0800

    购物按钮添加删除商品

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,20 +1,42 @@
 <template>
   <div class="cartcontrol">
-    <div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0"></div>
-    <div class="cart-count" v-show="food.count>0"></div>
-    <div class="cart-add icon-add_circle"></div>
+    <div @click="minusCart" class="cart-decrease icon-remove_circle_outline" v-show="food.count>0"></div>
+    <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
+    <div @click="addCart" class="cart-add icon-add_circle"></div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Vue from 'vue';
   export default {
     props: {
       food: {
         type: Object
       }
     },
-    created() {
-      console.log(this.food);
+    methods: {
+      minusCart(event) {
+        console.log('cli');
+        if (!event._constructed) {
+          return;
+        }
+        if (!this.food.count) {
+          Vue.set(this.food, 'count', 1);
+        } else {
+          this.food.count--;
+        }
+      },
+      addCart(event) {
+        console.log('cli');
+        if (!event._constructed) {
+          return;
+        }
+        if (!this.food.count) {
+          Vue.set(this.food, 'count', 1);
+        } else {
+          this.food.count++;
+        }
+      }
     }
   }
 </script>
@@ -31,6 +53,13 @@
       color: rgb(0, 160, 220)
     .cart-count
       display: inline-block
+      vertical-align: top
+      width: 12px
+      padding-top: 6px
+      line-height: 24px
+      text-align: center
+      font-size: 10px
+      color: rgb(147, 153, 159)
     .cart-add
       display: inline-block
 </style>
