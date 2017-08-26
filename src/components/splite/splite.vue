056e7263ad8e9f0a0af130e1d96d5d2ed6b8ed7e
Pepper <393928598@qq.com>
2017-08-26 15:26:14 +0800

    splte组件

---
+++ src/components/splite/splite.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="splite"></div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .splite
+    width: 100%
+    height: 16px
+    border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+    border-top: 1px solid rgba(7, 17, 27, 0.1)
+    background: #f3f5f7
+</style>
